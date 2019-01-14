c0db6543e9b0d830bbed96e874f97ce80b3c149b
wangjin5201314 <1713160140@qq.com>
2019-01-14 22:11:33 +0800

    1314

---
+++ js/wodedingdan.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+let toplist = document.querySelectorAll(".quanbu-box .text div");
+let bottomlist = document.querySelector(".box");
+	toplist.forEach((val,index)=>{
+		val.onclick = function(){
+			bottomlist.style.left = -100*index + "vw";
+			toplist.forEach((ele)=>{
+				ele.classList.remove("xz");
+			});
+			this.classList.add("xz");
+		}
+	});
+}
+
